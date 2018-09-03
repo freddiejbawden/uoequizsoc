c2e69af74b49310c0d421bf32be230947b3ca8b5
Frederick Bawden <frederickbawden@Fredericks-MacBook-Air.local>
2018-09-03 19:48:48 +0100

    Added Templates and finalised draft

--- js/constant.js
+++ js/constant.js
@@ -4,11 +4,26 @@ function openNav() {
 }
 function closeNav() {
   document.getElementById("sideNav").style.width = "0";
+  
 }
 $(document).click(function(e) {
   if(!$(e.target).closest('#sideNav').length) {
     if (($('#sideNav').css('width') != '0px')) {
       closeNav();
   }
+
 }
+});
+
+var resizeTimer;
+
+$(window).on('resize', function(e) {
+
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(function() {
+    location.reload();
+    // Run code here, resizing has "stopped"
+
+  }, 250);
+
 });
---
+++ js/index_scaling.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  changeBackdrop();
+});
+
+function changeBackdrop() {
+  if ($(window).width() < 840) {
+    $("html").css("background","");
+    $("#backdrop").attr("src","images/backdrop_small.jpg");
+    $("#main").css("margin", "10px 0 10px");
+  } else  {
+    $("#backdrop").removeAttr("src");
+    console.log( $('#topbar').height());
+    $("#main").height($(window).height() - $('#topbar').height());
+    $("html").css("background","url(images/backdrop_large.jpg) no-repeat center center");
+    $("#main").css("margin", "0");
+    //document.getElementById("backdrop").setAttribute("src","images/backdrop_large.jpg");
+  }
+}
